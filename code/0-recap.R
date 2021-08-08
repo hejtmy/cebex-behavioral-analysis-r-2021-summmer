4a3774d8989d0e3ac52a8d89d7b5a4047020656d
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-08-08 22:26:09 +0200

    Adds recap file

---
+++ code/0-recap.R
@@ -0,0 +1,13 @@
+#' Load airline safety data into a data frame. 
+#' Data are accessible in at https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv. 
+#' Description of variables can be found here: https://github.com/fivethirtyeight/data/tree/master/airline-safety
+#' 
+# How many variables and observations are in the dataset?
+# What is each column type? Get a "feel" for the data.
+# Print out dataset’s first 10 rows.
+# What is the variable type of indidents between 1985 and 1999?
+# How many unique airlines are there?
+# What is the average number and standard deviation of incidents for an airline between 85-99? Round to 3 digits.
+# What is the correlation between incidents in 85-99 and 00-14?
+# Plot the number of incidents in 00-14 vs 85-99
+# Plot the histogram of incidents in 00-14. Use 15 breaks
